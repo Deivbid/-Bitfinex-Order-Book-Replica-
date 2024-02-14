29d2bc81411d8f0d5eaad93bbebd0b9a6c76e72d
David Aparicio <davidaapariciobak@gmail.com>
2024-02-14 20:51:03 -0300

    Init: Start empty project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Card, CardContent } from "@/components/ui/card";
+
+function App() {
+  return (
+    <div className="min-h-screen w-full py-12 flex justify-center items-center">
+      <div className="relative w-full max-w-6xl mx-auto text-white flex justify-center items-center">
+        <Card>
+          <CardContent>AQUI IRA LA COSA ESA</CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+export default App;
